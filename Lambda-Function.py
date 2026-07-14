7563eee1be3f2f087898f1ade6ffce05bbb46521
Vinay Sharma <163511722+009vinaysharma@users.noreply.github.com>
2026-07-14 19:56:59 +0530

    Add files via upload

---
+++ Lambda-Function.py
@@ -0,0 +1,26 @@
+import json
+import boto3
+
+runtime = boto3.client("sagemaker-runtime")
+
+ENDPOINT_NAME = "sagemaker-scikit-learn-2026-07-14-13-18-30-285"
+
+def lambda_handler(event, context):
+
+    body = json.loads(event["body"])
+
+    response = runtime.invoke_endpoint(
+        EndpointName=ENDPOINT_NAME,
+        ContentType="application/json",
+        Body=json.dumps(body)
+    )
+
+    prediction = response["Body"].read().decode()
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": prediction
+    }
